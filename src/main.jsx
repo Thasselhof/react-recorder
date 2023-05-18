5a2f2a93b1eef9a0ead6eb6a76e77ea23e01ba2b
Daniel <thasselhof@gmail.com>
2023-05-18 09:15:05 +0200

    trying new way of autopla

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import AudioRecorder  from './AudioRecorder.jsx'
 import './index.css'
 import VideoPlayer from './VideoPlayer.jsx'
+import VideoPlayer2 from './VideoPlayer2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AudioRecorder />
     <VideoPlayer />
+    <VideoPlayer2 />
   </React.StrictMode>,
 )
