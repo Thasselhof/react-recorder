de7574c5d0ceae5b47e64e4b248eb38de8e2511e
Daniel <thasselhof@gmail.com>
2023-05-18 08:56:31 +0200

    added video

---
+++ src/VideoPlayer.jsx
@@ -0,0 +1,20 @@
+import ReactPlayer from 'react-player'
+
+  const VideoPlayer = () => {
+    return(
+      <div>
+      <ReactPlayer
+        url='https://rili.ai/avatares/videos_final/01.mp4'
+        className='react-player'
+        playing ={true}
+        width='100%'
+        height='100%'
+        playsinline={true}
+        muted={true}
+      />
+    </div>
+    )
+  };
+
+  export default VideoPlayer;
+  
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import AudioRecorder  from './AudioRecorder.jsx'
 import './index.css'
+import VideoPlayer from './VideoPlayer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AudioRecorder />
+    <VideoPlayer />
   </React.StrictMode>,
 )
