d985348caab60cfdf8165b1fa3ab7fe3b85d82ee
Daniel <thasselhof@gmail.com>
2023-05-18 09:09:05 +0200

    stoped autoplay and added buttons

--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -6,12 +6,13 @@ import ReactPlayer from 'react-player'
       <ReactPlayer
         url='https://rili.ai/avatares/videos_final/01.mp4'
         className='react-player'
-        playing ={true}
+        playing ={false}
         width='100%'
         height='100%'
         playsinline={true}
         muted={true}
         loop={true}
+        controls={true}
       />
     </div>
     )
